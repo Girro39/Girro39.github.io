b91394ad63a538e52da3a427d4706a8ad2bc8fe1
Girro39 <yellyou39@gmail.com>
2021-07-18 03:41:12 +0800

    js未完

---
+++ JS_HW/js_HW_09_Calendar/js_hw_09_calender.js
@@ -0,0 +1,7 @@
+let dateObj_Now = new Date();//現在日期時間
+console.log(dateObj_Now);
+
+console.log(dateObj_Now.getFullYear());//西元幾年
+console.log(dateObj_Now.getDate());//幾日
+console.log(dateObj_Now.getDay());//星期幾
+console.log(dateObj_Now.getMonth()+1);//幾月
