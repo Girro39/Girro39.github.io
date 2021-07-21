41e5efd17ed8d87a9e97534c31da1fdedc297dec
Girro39 <yellyou39@gmail.com>
2021-07-21 23:44:05 +0800

    0721

--- JS_HW/js_HW_09_Calendar/js_hw_09_calender.js
+++ JS_HW/js_HW_09_Calendar/js_hw_09_calender.js
@@ -1,7 +1,56 @@
-let dateObj_Now = new Date();//現在日期時間
-console.log(dateObj_Now);
+let dateObj_current = new Date();//依據電腦位置照，找出現在日期時間物件
+//#region 
+// console.log(dateObj_current instanceof Object);////是物件
+// console.log(dateObj_current);
+// console.log(current_year);
+// console.log(current_month);
+// console.log(current_date);
+// console.log(current_day);
+//#endregion
 
-console.log(dateObj_Now.getFullYear());//西元幾年
-console.log(dateObj_Now.getDate());//幾日
-console.log(dateObj_Now.getDay());//星期幾
-console.log(dateObj_Now.getMonth()+1);//幾月
+let year_month = document.getElementById('year_month');
+let previous_btn = document.getElementById('previous_btn');
+let next_btn = document.getElementById('next_btn');
+let today_btn =document.getElementById('today');
+let tds = document.querySelectorAll('tbody td');
+
+window.onload=function(){
+    init();
+    today_btn.addEventListener('click',function(){
+        init();
+    });
+
+}
+let firstDate;
+function init(){
+    year_month.innerHTML = 
+        `${dateObj_current.getFullYear()}年${dateObj_current.getMonth()+1}月`;
+    firstDate = new Date(dateObj_current.getFullYear(),dateObj_current.getMonth(),1);
+    // debugger;
+    firstDate.setDate(firstDate.getDate()-firstDate.getDay());
+    tds.forEach(item=>{
+        // debugger;
+        item.setAttribute('class','');
+        if(firstDate.getMonth()!==dateObj_current.getMonth()){
+            item.setAttribute('class','PNMonth');
+        }
+        item.innerHTML = firstDate.getDate();
+        firstDate.setDate(firstDate.getDate()+1);
+    });
+}
+
+function previousMonth(){
+    dateObj_current.setMonth(dateObj_current.getMonth()-1);
+    console.log(dateObj_current.getMonth());
+    init();
+}
+
+function nextMonth(){
+    dateObj_current.setMonth(dateObj_current.getMonth()+1);
+    console.log(dateObj_current.getMonth());
+    init();
+}
+previous_btn.addEventListener('click',previousMonth);
+next_btn.addEventListener('click',nextMonth);
+
+
