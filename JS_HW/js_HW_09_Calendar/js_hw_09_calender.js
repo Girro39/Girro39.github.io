095f5cdcea61638133cee295183e2d122664bf7c
Girro39 <yellyou39@gmail.com>
2021-07-22 12:56:22 +0800

    0722 localstroage未完成

--- JS_HW/js_HW_09_Calendar/js_hw_09_calender.js
+++ JS_HW/js_HW_09_Calendar/js_hw_09_calender.js
@@ -1,54 +1,65 @@
-let dateObj_current = new Date();//依據電腦位置照，找出現在日期時間物件
-//#region 
-// console.log(dateObj_current instanceof Object);////是物件
-// console.log(dateObj_current);
-// console.log(current_year);
-// console.log(current_month);
-// console.log(current_date);
-// console.log(current_day);
-//#endregion
+
 let year_month = document.getElementById('year_month');
 let previous_btn = document.getElementById('previous_btn');
 let next_btn = document.getElementById('next_btn');
 let today_btn =document.getElementById('today');
 let tds = document.querySelectorAll('tbody td');
-window.onload=function(){
-    init();
-    today_btn.addEventListener('click',function(){
-        init();
-    });
 
+let today = new Date();//用來跟controller比較
+let firstDate;//當月第一天
+let controller = new Date();//依據電腦位置照，找出現在日期時間物件，拿來控制firstDay成某月第一天
+
+window.onload=function(){
+    init();    
 }
-let firstDate;
+today_btn.addEventListener('click',function(){
+    controller = new Date();
+    init();
+});
+previous_btn.addEventListener('click',previousMonth);
+next_btn.addEventListener('click',nextMonth);
+
+tds.forEach(item=>{
+    item.setAttribute('data-toggle','modal');
+    item.setAttribute('data-target','#exampleModal');
+});
+
 function init(){
+
     year_month.innerHTML = 
-        `${dateObj_current.getFullYear()}年${dateObj_current.getMonth()+1}月`;
-    firstDate = new Date(dateObj_current.getFullYear(),dateObj_current.getMonth(),1);
+    `${controller.getFullYear()}/${controller.getMonth()+1}`;
     // debugger;
+    firstDate = new Date(controller.getFullYear(),controller.getMonth(),1);
     firstDate.setDate(firstDate.getDate()-firstDate.getDay());
     tds.forEach(item=>{
         // debugger;
+        // console.log(item);
+        // console.log(controller);
         item.setAttribute('class','');
-        if(firstDate.getMonth()!==dateObj_current.getMonth()){
+        if(firstDate.getMonth()!==controller.getMonth()){
             item.setAttribute('class','PNMonth');
         }
         item.innerHTML = firstDate.getDate();
+        item.setAttribute('value',`${firstDate.getMonth()}`);
+        
+        if(item.getAttribute('value')==today.getMonth() && item.textContent==today.getDate()){
+            let span=document.createElement('span');
+            span.innerHTML = `today`;
+            span.setAttribute('class','pl-5')
+            item.append(span);
+        }
         firstDate.setDate(firstDate.getDate()+1);
     });
 }
 
 function previousMonth(){
-    dateObj_current.setMonth(dateObj_current.getMonth()-1);
-    console.log(dateObj_current.getMonth());
+    controller.setMonth(controller.getMonth()-1);
     init();
 }
 
 function nextMonth(){
-    dateObj_current.setMonth(dateObj_current.getMonth()+1);
-    console.log(dateObj_current.getMonth());
+    controller.setMonth(controller.getMonth()+1);
     init();
 }
-previous_btn.addEventListener('click',previousMonth);
-next_btn.addEventListener('click',nextMonth);
 
 
